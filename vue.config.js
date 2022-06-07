6d3f62254739a249d2d1a805d624dbaf6344ef0d
YC <saiyuki0804@gmail.com>
2022-06-07 21:17:45 +0800

    [YC] 設定後台的port

--- vue.config.js
+++ vue.config.js
@@ -4,11 +4,21 @@ module.exports = {
   configureWebpack: {
     //Necessary to run npm link https://webpack.js.org/configuration/resolve/#resolve-symlinks
     resolve: {
-       symlinks: false
-    }
+      symlinks: false,
+    },
   },
-  transpileDependencies: [
-    '@coreui/utils',
-    '@coreui/vue'
-  ]
-}
+  devServer: {
+    host: "localhost",
+    port: 8082,
+    // https: true,
+    proxy: {
+      "/server": {
+        target: "http://localhost:8080/ita/",
+        pathRewrite: { "^/server": "" },
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  transpileDependencies: ["@coreui/utils", "@coreui/vue"],
+};
