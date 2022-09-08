1eac37dd0c8b1cb496efaedb20c09ae1d5a0add9
YC <saiyuki0804@gmail.com>
2022-09-08 16:40:32 +0800

    [YC] 調整餐點類別不儲存在vuex

--- src/store.js
+++ src/store.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import MenuCategory from "@/classes/MenuCategory";
 import AjaxService from "@/services/ajaxService.js";
 Vue.use(Vuex);
 
@@ -8,7 +7,6 @@ const state = {
   sidebarShow: "responsive",
   sidebarMinimize: false,
   globalLoading: false,
-  menuCategory: null,
 };
 
 const mutations = {
@@ -25,52 +23,11 @@ const mutations = {
   },
   // 初始化餐點類別
   initMenuCategory(state, dataList) {
-    state.menuCategory = new MenuCategory(dataList);
+    state.menuCategory.init(dataList);
   },
 };
 
-const actions = {
-  queryMenuCategory(context) {
-    if (!context.menuCategory) {
-      new Promise((resolve, reject) => {
-        AjaxService.get(
-          "/server/mealCate/cate",
-          (successResp) => {
-            if (successResp.restData) {
-              resolve(successResp.restData);
-              console.log("查詢所有餐點類別成功");
-            }
-          },
-          (errorResp) => {
-            reject(errorResp);
-            console.log("查詢所有餐點類別失敗");
-          }
-        );
-      })
-        .then((category) => {
-          return new Promise((resolve, reject) => {
-            AjaxService.get(
-              "/server/mealCate/sub",
-              (successResp) => {
-                if (successResp.restData) {
-                  let subCategory = successResp.restData;
-                  context.commit("initMenuCategory", [category, subCategory]);
-                  console.log("查詢所有餐點子類別成功");
-                }
-              },
-              (errorResp) => {
-                reject(errorResp);
-                console.log("查詢所有餐點子類別失敗");
-              }
-            );
-          });
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-  },
-};
+const actions = {};
 
 export default new Vuex.Store({
   state,
