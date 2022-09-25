9935b09ce03973c4367d56059994b1f63c052d5b
YC <saiyuki0804@gmail.com>
2022-09-25 15:47:33 +0800

    [YC] 側邊欄隱藏不需要的頁面

--- src/containers/_nav.js
+++ src/containers/_nav.js
@@ -46,16 +46,16 @@ export default [
         to: "/mngt/news",
         icon: "cilBullhorn",
       },
-      {
-        _name: "CSidebarNavTitle",
-        _children: ["設定"],
-      },
-      {
-        _name: "CSidebarNavItem",
-        name: "基本設定",
-        to: "/st/basic",
-        icon: "cilLeaf",
-      },
+      // {
+      //   _name: "CSidebarNavTitle",
+      //   _children: ["設定"],
+      // },
+      // {
+      //   _name: "CSidebarNavItem",
+      //   name: "基本設定",
+      //   to: "/st/basic",
+      //   icon: "cilLeaf",
+      // },
       {
         _name: "CSidebarNavTitle",
         _children: ["圖表分析"],
@@ -70,40 +70,40 @@ export default [
         _name: "CSidebarNavDivider",
         _class: "m-2",
       },
-      {
-        _name: "CSidebarNavTitle",
-        _children: ["測試參考用"],
-      },
-      {
-        _name: "CSidebarNavItem",
-        name: "Color Reference",
-        to: "/pages/colors",
-        icon: "cil-bell",
-      },
-      {
-        _name: "CSidebarNavDropdown",
-        name: "Pages",
-        route: "/pages",
-        icon: "cil-star",
-        items: [
-          {
-            name: "Login",
-            to: "/pages/login",
-          },
-          {
-            name: "Register",
-            to: "/pages/register",
-          },
-          {
-            name: "Error 404",
-            to: "/pages/404",
-          },
-          {
-            name: "Error 500",
-            to: "/pages/500",
-          },
-        ],
-      },
+      // {
+      //   _name: "CSidebarNavTitle",
+      //   _children: ["測試參考用"],
+      // },
+      // {
+      //   _name: "CSidebarNavItem",
+      //   name: "Color Reference",
+      //   to: "/pages/colors",
+      //   icon: "cil-bell",
+      // },
+      // {
+      //   _name: "CSidebarNavDropdown",
+      //   name: "Pages",
+      //   route: "/pages",
+      //   icon: "cil-star",
+      //   items: [
+      //     {
+      //       name: "Login",
+      //       to: "/pages/login",
+      //     },
+      //     {
+      //       name: "Register",
+      //       to: "/pages/register",
+      //     },
+      //     {
+      //       name: "Error 404",
+      //       to: "/pages/404",
+      //     },
+      //     {
+      //       name: "Error 500",
+      //       to: "/pages/500",
+      //     },
+      //   ],
+      // },
       // {
       //   _name: "CSidebarNavItem",
       //   name: "Download CoreUI",
